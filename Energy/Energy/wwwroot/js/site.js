d6921c60b9da103b681c08789b53aa4774c63c7a
Bilal Haroon <bilalharoon7@gmail.com>
2019-06-01 22:08:28 -0400

    options

--- Energy/Energy/wwwroot/js/site.js
+++ Energy/Energy/wwwroot/js/site.js
@@ -2,3 +2,33 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+function carBox() {
+    var result = document.getElementById("vehicle").checked; 
+    if (result) {
+        document.getElementById("fuel type").disabled = false;
+    }
+    else {
+        document.getElementById("fuel type").disabled = true;
+    }
+}
+
+function walkBox() {
+    var result = document.getElementById("feet").checked;
+    if (result) {
+        document.getElementById("walkBox").disabled = false;
+    }
+    else {
+        document.getElementById("walkBox").disabled = true;
+    }
+}
+
+function bikeBox() {
+    var result = document.getElementById("pedal").checked;
+    if (result) {
+        document.getElementById("bikeBox").disabled = false;
+    }
+    else {
+        document.getElementById("bikeBox").disabled = true;
+    }
+}
